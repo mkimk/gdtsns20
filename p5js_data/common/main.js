812af150939605cab42b8805a8ed83b73568af42
Minkyoung Kim <mineh.kim@gmail.com>
2020-05-08 23:27:25 -0400

    updated js/json files after the workshop

--- p5js_data/common/main.js
+++ p5js_data/common/main.js
@@ -1,8 +1,31 @@
+
+var weatherData;
+var url ="https://api.openweathermap.org/data/2.5/weather?q=";
+var city = "London";
+var apiKey ="&appid=da41b776483f6b58fc0dc17d649e9ab1";
+var button;
+var input;
+
 function setup(){
-    createCanvas(200,200);
+    createCanvas(1400,700);
+    input = createInput('London');
+    button = createButton('submit');
+    button.mousePressed(functionY);
 }
+
+function functionX(data) {
+    weatherData = data;
+  }
+
+function functionY() {
+    loadJSON(url + input.value() + apiKey, functionX, 'jsonp');
+}
+
 function draw(){
     background(0);
-    fill(255);
-    text('test', 50,50);
+    if (weatherData) {
+        fill(255);
+        circle(700,700, weatherData.main.temp * 2);
+        console.log(weatherData);
+    }
 }
